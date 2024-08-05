83fad410dd841cfa788f0301c845f0d6513651e0
RAFAEL CARDOSO DONDE <rafael_donde@estudante.sesisenai.org.br>
2024-08-05 12:07:27 -0300

    Estrutura Básica e Componentes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+// Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return <footer><p>
+ 2024 Viagem App</p></footer>;
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+// Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return <header><h1>Aplicativo de Viagens</h1></header>;
+};
+
+export default Header;
